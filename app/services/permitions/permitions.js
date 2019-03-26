54cfd945cf2ecc0fe36e202eef21c98c7a23577a
Артем Соколенко <artems@wizardsdev.com>
2019-03-26 06:41:12 -0700

    run ios

--- app/services/permitions/permitions.js
+++ app/services/permitions/permitions.js
@@ -4,32 +4,31 @@ import {
   ToastAndroid,
 } from 'react-native';
 
-export default class PermissionForLocation
-{
-	static hasLocationPermission = async () => {
+export default class PermissionForLocation {
+  static hasLocationPermission = async () => {
     let result = false;
 
-    if (Platform.OS === 'ios'){
+    if (Platform.OS === 'ios') {
       result = true;
+    } else {
+      const hasPermission = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+
+      if (hasPermission) result = true;
+
+      const status = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+
+      if (status === PermissionsAndroid.RESULTS.GRANTED) result = true;
+
+      if (status === PermissionsAndroid.RESULTS.DENIED) {
+        ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
+      } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+        ToastAndroid.show('Location permission revoked by user.', ToastAndroid.LONG);
+      }
     }
-    const hasPermission = await PermissionsAndroid.check(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-    );
-
-    if (hasPermission) result = true;
-
-    const status = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-    );
-
-    if (status === PermissionsAndroid.RESULTS.GRANTED) result = true;
-
-    if (status === PermissionsAndroid.RESULTS.DENIED) {
-      ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
-    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
-      ToastAndroid.show('Location permission revoked by user.', ToastAndroid.LONG);
-    }
-
     return result;
   }
 }
